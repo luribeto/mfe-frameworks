cf445c86192c1540b82d84bc9845874e35c53d37
Leonardo Uribe T <leonardouribet@LATAM-ZP7XWM.local>
2024-06-05 08:43:28 -0500

    mounting MarketingApp

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,28 +1,45 @@
 import React from 'react';
-import { Switch, Route, Router } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import {
   StylesProvider,
-  createGenerateClassName,
 } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-const generateClassName = createGenerateClassName({
-  productionPrefix: 'ma',
-});
 
-export default ({ history }) => {
+export default () => {
   return (
     <div>
-      <StylesProvider generateClassName={generateClassName}>
-        <Router history={history}>
+      <StylesProvider>
+        <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route path="/" component={Landing} />
           </Switch>
-        </Router>
+        </BrowserRouter>
       </StylesProvider>
     </div>
   );
 };
+
+
+
+
+
+
+
+// export default ({ history }) => {
+//   return (
+//     <div>
+//       <StylesProvider generateClassName={generateClassName}>
+//         <Router history={history}>
+//           <Switch>
+//             <Route exact path="/pricing" component={Pricing} />
+//             <Route path="/" component={Landing} />
+//           </Switch>
+//         </Router>
+//       </StylesProvider>
+//     </div>
+//   );
+// };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,45 +1,22 @@
-console.log("Marketing Running!!")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { createMemoryHistory, createBrowserHistory } from 'history';
-// import App from './App';
+// Mount function to start up the app
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
 
-// // Mount function to start up the app
-// const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
-//   const history =
-//     defaultHistory ||
-//     createMemoryHistory({
-//       initialEntries: [initialPath],
-//     });
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
 
-//   if (onNavigate) {
-//     history.listen(onNavigate);
-//   }
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
 
-//   ReactDOM.render(<App history={history} />, el);
-
-//   return {
-//     onParentNavigate({ pathname: nextPathname }) {
-//       const { pathname } = history.location;
-
-//       if (pathname !== nextPathname) {
-//         history.push(nextPathname);
-//       }
-//     },
-//   };
-// };
-
-// // If we are in development and in isolation,
-// // call mount immediately
-// if (process.env.NODE_ENV === 'development') {
-//   const devRoot = document.querySelector('#_marketing-dev-root');
-
-//   if (devRoot) {
-//     mount(devRoot, { defaultHistory: createBrowserHistory() });
-//   }
-// }
-
-// // We are running through container
-// // and we should export the mount function
-// export { mount };
+// We are running through container
+// and we should export the mount function
+export { mount };
