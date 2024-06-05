293ac3bc1891cbb865b0cddce9e55622b1e65e38
Leonardo Uribe T <leonardouribet@LATAM-ZP7XWM.local>
2024-06-05 15:39:45 -0500

    Pricing click to CONT

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createMemoryHistory, createBrowserHistory } from 'history';
+import { createMemoryHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
   ReactDOM.render(<App history={history} />, el);
 };
 
@@ -14,7 +19,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {  });
   }
 }
 
